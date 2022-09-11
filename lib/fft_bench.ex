3762d14bb4905bda28a74d4288ca7b366c4090ff
Susumu Yamazaki <zacky1972@gmail.com>
2022-09-11 10:31:41 +0900

    initial commit

---
+++ lib/fft_bench.ex
@@ -0,0 +1,18 @@
+defmodule FftBench do
+  @moduledoc """
+  Documentation for `FftBench`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FftBench.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/fft_bench_test.exs
@@ -0,0 +1,8 @@
+defmodule FftBenchTest do
+  use ExUnit.Case
+  doctest FftBench
+
+  test "greets the world" do
+    assert FftBench.hello() == :world
+  end
+end
